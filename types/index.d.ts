247aef1493e5958edb0c2b130b74971c8160e3d4
mateonunez <mateonunez95@gmail.com>
2023-10-29 21:08:08 +0100

    feat(type): improve types and add query type
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-import { ConnectionPool, ConnectionPoolConfig } from "@databases/mysql";
+import { Connection, ConnectionPool, ConnectionPoolConfig, Transaction } from "@databases/mysql";
 import { SQL } from "@databases/sql";
 import { FastifyPluginCallback } from "fastify";
 
@@ -8,22 +8,34 @@ interface FastifyAtMysqlPluginOptions extends ConnectionPoolConfig {
   user: string;
   password: string;
   database: string;
+  name?: string;
 }
 
 declare const fastifyAtMysql: FastifyPluginCallback<FastifyAtMysqlPluginOptions>;
 
-type Queryable = {
-  mysql: {
-    query<T>(query: string): Promise<T[]>;
-    transaction<T>(queries: string[]): Promise<T>;
-    sql: SQL;
-    db: ConnectionPool;
-  };
+type QueryType = "raw" | "iterable" | "stream";
+
+type QueryOptions = {
+  type?: QueryType;
+};
+
+type MysqlInstance = {
+  query<T>(query: string, options?: QueryOptions): QueryOptions["type"] extends "iterable"
+    ? AsyncIterable<T>
+    : QueryOptions["type"] extends "stream"
+    ? NodeJS.ReadableStream
+    : Promise<T[]>;
+  transaction<T>(fn: (connection: Transaction) => Promise<T>): Promise<T>;
+  task<T>(fn: (connection: Connection | Transaction) => Promise<T>): Promise<T>;
+  sql: SQL;
+  db: ConnectionPool;
 };
 
 declare module "fastify" {
   interface FastifyInstance {
-    mysql: Queryable["mysql"];
+    mysql: MysqlInstance & {
+      [instanceName: string]: MysqlInstance;
+    };
   }
 }
 
